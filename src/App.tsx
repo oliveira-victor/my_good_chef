cfd0c6e6fcd6a959096dc6dc848ede3297caa7ea
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-04-29 10:19:29 -0300

    feat: react router scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./containers/Footer";
-import Header from "./containers/Header";
-import GlobalStyle, { Wrapper } from "./styles";
-import Pages from "./routes";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import Pages from "./routes";
 import { store } from "./store";
+import Header from "./containers/Header";
 import PhoneMenu from "./containers/PhoneMenu";
 import Favorites from "./containers/Favorites";
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./containers/Footer";
+
+import GlobalStyle, { Wrapper } from "./styles";
 
 function App() {
 
   return (
     <Provider store={store}>
       <BrowserRouter>
+      <ScrollToTop />
         <GlobalStyle />
         <Wrapper>
           <Header />
